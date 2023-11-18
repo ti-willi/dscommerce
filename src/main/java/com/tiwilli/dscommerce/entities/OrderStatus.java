2d4ad904789a0f99a0fbe289e804489649f9ac8d
Willian Comiotto <willianbcomiotto@gmail.com>
2023-11-18 13:57:22 -0300

    Entities User and Order

---
+++ src/main/java/com/tiwilli/dscommerce/entities/OrderStatus.java
@@ -0,0 +1,7 @@
+package com.tiwilli.dscommerce.entities;
+
+public enum OrderStatus {
+
+    WAITING_PAYMENT, PAID, SHIPPED, DELIVERED, CANCELED;
+
+}
---
+++ src/test/java/com/tiwilli/dscommerce/DscommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiwilli.dscommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
