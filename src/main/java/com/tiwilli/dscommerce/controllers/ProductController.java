c804ae10265664176540a45e544c386da85f2721
Willian Comiotto <willianbcomiotto@gmail.com>
2023-11-23 12:45:29 -0300

    DELETE Product

--- src/main/java/com/tiwilli/dscommerce/controllers/ProductController.java
+++ src/main/java/com/tiwilli/dscommerce/controllers/ProductController.java
@@ -2,6 +2,7 @@ package com.tiwilli.dscommerce.controllers;
 
 import com.tiwilli.dscommerce.dto.ProductDTO;
 import com.tiwilli.dscommerce.services.ProductService;
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -43,5 +44,11 @@ public class ProductController {
     public ResponseEntity<ProductDTO> update(@PathVariable Long id, @RequestBody ProductDTO dto) {
         dto = service.update(id, dto);
         return ResponseEntity.ok(dto);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
     }
 }
