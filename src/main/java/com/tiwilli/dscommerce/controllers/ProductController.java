144295f7d5d37f330ff931c56923b474b9ead91a
Willian Comiotto <willianbcomiotto@gmail.com>
2023-11-23 21:04:40 -0300

    Bean Validation

--- src/main/java/com/tiwilli/dscommerce/controllers/ProductController.java
+++ src/main/java/com/tiwilli/dscommerce/controllers/ProductController.java
@@ -2,6 +2,7 @@ package com.tiwilli.dscommerce.controllers;
 
 import com.tiwilli.dscommerce.dto.ProductDTO;
 import com.tiwilli.dscommerce.services.ProductService;
+import jakarta.validation.Valid;
 import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -33,7 +34,7 @@ public class ProductController {
     }
 
     @PostMapping
-    public ResponseEntity<ProductDTO> insert(@RequestBody ProductDTO dto) {
+    public ResponseEntity<ProductDTO> insert(@Valid @RequestBody ProductDTO dto) {
         dto = service.insert(dto);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
                 .buildAndExpand(dto.getId()).toUri();
@@ -41,7 +42,7 @@ public class ProductController {
     }
 
     @PutMapping(value = "/{id}")
-    public ResponseEntity<ProductDTO> update(@PathVariable Long id, @RequestBody ProductDTO dto) {
+    public ResponseEntity<ProductDTO> update(@PathVariable Long id, @Valid @RequestBody ProductDTO dto) {
         dto = service.update(id, dto);
         return ResponseEntity.ok(dto);
     }
