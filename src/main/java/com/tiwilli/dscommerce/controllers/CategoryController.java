1bc1c39431c6187e06f88bc9bd3f51a737b0583c
Willian Comiotto <willianbcomiotto@gmail.com>
2024-01-04 11:45:48 -0300

    GET Category

---
+++ src/main/java/com/tiwilli/dscommerce/controllers/CategoryController.java
@@ -0,0 +1,26 @@
+package com.tiwilli.dscommerce.controllers;
+
+import com.tiwilli.dscommerce.dto.CategoryDTO;
+import com.tiwilli.dscommerce.services.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/categories")
+public class CategoryController {
+
+    @Autowired
+    private CategoryService service;
+
+    @GetMapping
+    public ResponseEntity<List<CategoryDTO>> findAll() {
+        List<CategoryDTO> dto = service.findAll();
+        return ResponseEntity.ok(dto);
+    }
+
+}
