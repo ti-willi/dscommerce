bd530d19060437af6f08978c1ee0615c86ec6b06
Willian Comiotto <willianbcomiotto@gmail.com>
2024-01-04 11:56:54 -0300

    Fix validation

--- src/main/java/com/tiwilli/dscommerce/dto/ProductDTO.java
+++ src/main/java/com/tiwilli/dscommerce/dto/ProductDTO.java
@@ -2,10 +2,7 @@ package com.tiwilli.dscommerce.dto;
 
 import com.tiwilli.dscommerce.entities.Category;
 import com.tiwilli.dscommerce.entities.Product;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.Positive;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,6 +15,7 @@ public class ProductDTO {
     private String name;
     @Size(min = 10, message = "Descrição precisa ter no mínimo 10 caracteres")
     private String description;
+    @NotNull(message = "Campo requerido")
     @Positive(message = "O preço deve ser positivo")
     private Double price;
     private String imgUrl;
